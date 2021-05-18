168d6308822488127a1b9661f250549e00e36323
Ian Ornelas <ian.orn@gmail.com>
2021-05-18 15:54:41 -0300

    Update lint and configuration files

--- types/open-file-explorer.d.ts
+++ types/open-file-explorer.d.ts
@@ -1,4 +1,4 @@
 declare module 'open-file-explorer' {
-    function openExplorer(path: string, callback?: (err: Error) => any): void;
-    export = openExplorer;
+  function openExplorer(path: string, callback?: (err: Error) => void): void;
+  export = openExplorer;
 }
